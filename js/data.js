8c2c442d417fa6c661d00a99dfba4a30fe427a6c
Kenneth Wang <yunfeiw@me.com>
2021-09-07 15:57:30 -0700

    The new entries are saved to local storage

--- js/data.js
+++ js/data.js
@@ -1,8 +1,45 @@
 /* exported data */
+var $photoUrl = document.querySelector('#photoUrl');
+var $picView = document.querySelector('.picView');
 
+console.log($photoUrl),
+console.log($picView);
+
+function showPicture(event) {
+  if (event.target.value !== '') {
+    $picView.setAttribute('src', event.target.value);
+  }
+}
+
+$photoUrl.addEventListener('input', showPicture);
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+var $entryForm = document.querySelector('#entryForm');
+
+console.log($entryForm);
+var nextEntryId = 0;
+
+$entryForm.addEventListener('submit', function () {
+  event.preventDefault();
+  nextEntryId++;
+  var entryData = {};
+  entryData.title = $entryForm.elements.title.value;
+  entryData.photoUrl = $entryForm.elements.photoUrl.value;
+  entryData.notes = $entryForm.elements.notes.value;
+  entryData.nextEntryId = nextEntryId;
+  data.entries.push(entryData);
+  console.log(entryData);
+  console.log(data);
+  $picView.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryForm.reset();
+});
+
+window.addEventListener('beforeunload', function () {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+});
