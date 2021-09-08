262f905c130e1c897d82ae8912579252adae820f
Kenneth Wang <yunfeiw@me.com>
2021-09-07 18:02:20 -0700

    Correct the entryId for each dataEntry and keep JS codes in different file

--- js/data.js
+++ js/data.js
@@ -1,14 +1,4 @@
 /* exported data */
-var $photoUrl = document.querySelector('#photoUrl');
-var $picView = document.querySelector('.picView');
-
-function showPicture(event) {
-  if (event.target.value !== '') {
-    $picView.setAttribute('src', event.target.value);
-  }
-}
-
-$photoUrl.addEventListener('input', showPicture);
 
 var data = {
   view: 'entry-form',
@@ -16,23 +6,6 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
-
-var $entryForm = document.querySelector('#entryForm');
-var entryId = 0;
-
-$entryForm.addEventListener('submit', function () {
-  event.preventDefault();
-  entryId++;
-  var entryData = {};
-  entryData.title = $entryForm.elements.title.value;
-  entryData.photoUrl = $entryForm.elements.photoUrl.value;
-  entryData.notes = $entryForm.elements.notes.value;
-  entryData.nextEntryId = entryId;
-  data.entries.push(entryData);
-  data.nextEntryId = entryId;
-  $picView.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $entryForm.reset();
-});
 
 window.addEventListener('beforeunload', function () {
   var dataJSON = JSON.stringify(data);
--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#photoUrl');
+var $picView = document.querySelector('.picView');
+
+function showPicture(event) {
+  if (event.target.value !== '') {
+    $picView.setAttribute('src', event.target.value);
+  }
+}
+
+$photoUrl.addEventListener('input', showPicture);
+
+var $entryForm = document.querySelector('#entryForm');
+
+$entryForm.addEventListener('submit', function () {
+  event.preventDefault();
+  var entryData = {};
+  entryData.title = $entryForm.elements.title.value;
+  entryData.photoUrl = $entryForm.elements.photoUrl.value;
+  entryData.notes = $entryForm.elements.notes.value;
+  entryData.entryId = data.nextEntryId;
+  data.entries.push(entryData);
+  data.nextEntryId++;
+  $picView.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryForm.reset();
+});
